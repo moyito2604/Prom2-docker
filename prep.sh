a3c8c56f9fd07dbaf7aca6b900e3d3f32baf549b
Mobro <39844165+moyito2604@users.noreply.github.com>
2024-06-02 14:51:14 -0400

    VARTMP removed
    Updated to automatically grab variables and replace them

--- prep.sh
+++ prep.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 FILENAME=prom2v285.zip
 #FOLDERNM="Prominence II [RPG] Server Pack v2.8.5"
-VARTMP="template.txt"
-VARPMT="variables.txt"
+VARIABLES="variables.txt"
 PROPTMP="server.properties.tmp"
-PROPPMT="server.properties"
+PROPERTIES="server.properties"
 URL="https://www.curseforge.com/api/v1/mods/466901/files/5380858/download"
 
 #Function to download modpack
@@ -24,24 +23,15 @@ cd /data
 #checks if there is a current modpack installation
 if [ -f "$FILENAME" ]; then
     echo "Installation Exists"
-    rm -rf $VARPMT
-
-    #Ensures there is a valid variable template file
-    if [ -f "$VARTMP" ]; then
-        cp $VARTMP $VARPMT
-    else
-        echo "Missing $VARTMP file: Unzipping modpack for replacement"
-        unzip_modpack
-        cp $VARPMT $VARTMP
-    fi
+    rm -rf $VARIABLES
 
     #Ensures there is a valid server.properties template file
     if [ -f "$PROPTMP" ]; then
-        echo "Valid $PROPPMT file"
+        echo "Valid $PROPERTIES file"
     else
         echo "Missing $PROPTMP file: Unzipping modpack for replacement"
         unzip_modpack
-        cp $PROPPMT $PROPTMP
+        cp $PROPERTIES $PROPTMP
     fi
 else
     #Clean up old files
@@ -50,8 +40,7 @@ else
     #If there is no installation, it downloads the modpack and creates the installation
     dl_modpack
     unzip_modpack
-    cp $VARPMT $VARTMP
-    cp $PROPPMT $PROPTMP
+    cp $PROPERTIES $PROPTMP
 fi
 
 #creates eula.txt file to accept minecraft's EULA
@@ -59,15 +48,16 @@ rm -rf eula.txt
 echo eula=true > eula.txt
 
 #Sets Variables
+OLDVARS=$(cat $VARIABLES | grep JAVA_ARGS)
 echo ""
 echo "Variables:"
-sed 's%JAVA_ARGS="-Xms1G -Xmx6G %JAVA_ARGS="'"$JAVA_ARGS "'%' $VARPMT
-sed -i 's%JAVA_ARGS="-Xms1G -Xmx6G %JAVA_ARGS="'"$JAVA_ARGS "'%' $VARPMT
+sed 's%$OLDVARS%JAVA_ARGS="'"$JAVA_ARGS "'%' $VARIABLES
+sed -i 's%$OLDVARS%JAVA_ARGS="'"$JAVA_ARGS "'%' $VARIABLES
 
-echo enable-rcon=true>>$PROPPMT
-echo rcon.password=$RCON_PASS>>$PROPPMT
-echo 'rcon.port=25575'>>$PROPPMT
-echo 'broadcast-rcon-to-ops=false'>>$PROPPMT
+echo enable-rcon=true>>$PROPERTIES
+echo rcon.password=$RCON_PASS>>$PROPERTIES
+echo 'rcon.port=25575'>>$PROPERTIES
+echo 'broadcast-rcon-to-ops=false'>>$PROPERTIES
 
 #Changes permissions and starts server
 chmod +x start.sh
